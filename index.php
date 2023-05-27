64a59072c8568390f868500f93bb1c56c6463591
rafaelcitario <contato.rafaelgomes@outlook.com>
2023-05-27 15:38:26 -0300

    get and set - FILTER_SANITIZER_EMAIL

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+/*
 class Pessoa {
   // atributos -> caracteristicas da pessoa
   public $nome;
@@ -10,9 +11,113 @@ class Pessoa {
   }
 }
 
-/*
+
 $rodrigo = new Pessoa();
 $rodrigo->nome = "Rodrigo Santos de Oliveira";
 $rodrigo->idade = 27;
 $rodrigo->falar();
 */
+
+
+// atributos publicos -----
+/* class Login {
+  public $email;
+  public $senha;
+
+  public function logar() {
+    if ($this->email == "teste@teste.com" && $this->senha == "123456") {
+      echo "Logado com sucesso!";
+    } else {
+      echo "Dados inválidos";
+    }
+  }
+}
+
+
+$logar = new Login();
+$logar->email = "teste@teste.com";
+$logar->senha = "1234567";
+$logar->logar();
+ */
+
+/* // atributos privados, utilizando get e set
+class Login {
+  private $email;
+  private $senha;
+
+  public function getEmail() {
+    return $this->email;
+  }
+  public function setEmail($e) {
+    $email = filter_var($e, FILTER_SANITIZE_EMAIL); // filtrando caracteres especiais no email.
+    $this->email = $email;
+  }
+  public function getSenha() {
+    return $this->senha;
+  }
+  public function setSenha($e) {
+    $this->senha = $e;
+  }
+
+  public function logar() {
+    if ($this->email == "teste@teste.com" && $this->senha == 123456) {
+      echo "Logado com sucesso!";
+    } else {
+      echo "Dados inválidos";
+    }
+  }
+}
+
+
+$logar = new Login();
+$logar->setEmail('teste@teste.com');
+$logar->setSenha(123456);
+
+$logar->logar();
+echo "<br>";
+$logar->getEmail();
+echo $logar->getEmail();
+echo "<br>";
+echo md5(md5($logar->getSenha())); // aqui dei uma brincadinha
+ */
+
+// PRATICANDO 
+/* class Login {
+  private $email;
+  private $senha;
+
+  public function getEmail() {
+    return $this->email;
+  }
+  public function getSenha() {
+    return $this->senha;
+  }
+
+  public function setEmail($e) {
+    $email = filter_var($e, FILTER_SANITIZE_EMAIL);
+    $this->email = $email;
+  }
+  public function setSenha($e) {
+    $this->senha = $e;
+  }
+
+  public function logar() {
+    if ($this->email == "teste@teste.com" && $this->senha == 123456) {
+      echo "Login efetuado com sucesso!";
+    } else {
+      echo "Dados invalidos, tente novamente ou entre em contato com o suporte.";
+    }
+  }
+}
+$conta = new Login();
+$conta->setEmail('teste@teste.co()\m'); // FILTER_SANITIZER_EMAIL retirou os caracteres "(")\"
+$conta->setSenha(123456);
+
+$conta->logar();
+
+print "<br>";
+echo $conta->getEmail();
+print "<br>";
+echo $conta->getSenha();
+ */
+// Constructor
